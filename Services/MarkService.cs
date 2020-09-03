0bd2dc3466b0febcee4615f2a22afed5efb919a5
BohdanKov <bohdankov@tahometer.com>
2020-09-03 16:47:56 +0300

    Added service Mark

---
+++ Services/MarkService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.DAL;
+using University.Models;
+
+namespace University.Services
+{
+    public class MarkService
+    {
+        UniversityContext db;
+
+        public MarkService(UniversityContext db)
+        {
+            this.db = db;
+        }
+    }
+}
