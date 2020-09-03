845741a6714787dc60355447458c5ffbe970348f
BohdanKov <bohdankov@tahometer.com>
2020-09-03 11:02:34 +0300

    Add StudentController

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using University.Services;
+using University.Models;
+using University.DAL;
+
+
+namespace University.Controllers
+{
+    
+    public class StudentController : Controller
+    {
+        
+        private StudentService studentService;
+
+        public StudentController(StudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+        [HttpGet("students")]
+        public List<Student> GetStudents()
+        {
+            Console.WriteLine(studentService.GetStudentsList());
+            return studentService.GetStudentsList();
+        }
+    }
+}
