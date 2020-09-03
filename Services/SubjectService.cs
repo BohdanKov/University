93ee53273c4ccea5ef0cf512552a8599918331f5
BohdanKov <bohdankov@tahometer.com>
2020-09-03 15:28:21 +0300

    Add Subject service

---
+++ Services/SubjectService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using University.DAL;
+using University.Models;
+
+namespace University.Services
+{
+    public class SubjectService
+    {
+        UniversityContext db;
+        public SubjectService(UniversityContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Subject> GetSubjectsList()
+        {
+            return db.Subjects.ToList();
+        }
+    }
+}
