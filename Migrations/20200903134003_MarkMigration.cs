cf342a994974030bea0f51988781b2bc86823147
BohdanKov <bohdankov@tahometer.com>
2020-09-03 16:42:40 +0300

    Added migration Mark

---
+++ Migrations/20200903134003_MarkMigration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace University.Migrations
+{
+    public partial class MarkMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marks",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentID = table.Column<int>(nullable: false),
+                    SubjectID = table.Column<int>(nullable: false),
+                    Grade = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marks", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Marks_Students_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Marks_Subjects_SubjectID",
+                        column: x => x.SubjectID,
+                        principalTable: "Subjects",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marks_StudentID",
+                table: "Marks",
+                column: "StudentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marks_SubjectID",
+                table: "Marks",
+                column: "SubjectID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marks");
+        }
+    }
+}
