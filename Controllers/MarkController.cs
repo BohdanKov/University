2974572a7b66419bba9a1950971a1c28aca4c2c4
BohdanKov <bohdankov@tahometer.com>
2020-09-03 16:56:48 +0300

    Added controller Mark

---
+++ Controllers/MarkController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using University.Services;
+using University.Models;
+using University.DAL;
+
+namespace University.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class MarkController : Controller
+    {
+        MarkService markService;
+
+        public MarkController(MarkService markService)
+        {
+            this.markService = markService;
+        }
+    }
+}
