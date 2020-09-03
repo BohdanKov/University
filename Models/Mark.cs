4a19bc18961b8ff5e538b72862b056f126cfc13f
BohdanKov <bohdankov@tahometer.com>
2020-09-03 16:38:01 +0300

    Added model Mark

---
+++ Models/Mark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Models
+{
+    public class Mark
+    {
+        public int ID { get; set; }
+        public int StudentID { get; set; }
+        public int SubjectID { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
+        public Student Student { get; set; }
+        public Subject Subject { get; set; }
+
+    }
+}
